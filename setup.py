ec030ba730b38d86486190e75c51be3657ca3ee6
Eric Holmgren <eholmgr2@u.rochester.edu>
2017-09-18 11:43:06 -0400

    made test

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='LangevinSim',
+      version='0.01',
+      description='Langevin Simulator',
+      author='Eric Holmgren',
+      author_email='eholmgr2@u.rochester.edu',
+      packages=['lans'],
+     )
