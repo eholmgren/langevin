e4498b8c0a2d607cc4663389175ccde3af10c1b8
Eric Holmgren <eholmgr2@u.rochester.edu>
2017-09-21 14:32:28 -0400

    now gets parameters from commandline

--- lans/lans.py
+++ lans/lans.py
@@ -1,5 +1,34 @@
+import argparse
+
+#defaults
+ENERGY = r'.\tests\potential_example.txt'
+POSITION = 0.0
+VELOCITY = 0.0
+TEMPERATURE = 100.0
+DAMPING = 1.0
+TIMESTEP = 0.1
+TOTALTIME = 100.0
+
+
+
+
+def get_arguments():
+    '''Get arguments from command line'''
+
+    parser = argparse.ArgumentParser(description='Langevin Dynamics Simulator')
+    parser.add_argument('--energy', type=str, default=ENERGY, help='File of Potential Energy')
+    parser.add_argument('--position', type=float, default=POSITION, help='Initial position')
+    parser.add_argument('--velocity', type=float, default=VELOCITY, help='Initial velocity')
+    parser.add_argument('--temperature', type=float, default=TEMPERATURE, help='Temperature')
+    parser.add_argument('--damping', type=float, default=DAMPING, help='Damping coefficient')
+    parser.add_argument('--timestep', type=float, default=TIMESTEP, help='Time step')
+    parser.add_argument('--totaltime', type=float, default=TOTALTIME, help='Total time')
+    
+    return parser.parse_args()
+
+
 def read_energy(input_file):
-    '''Reads in the energy file and returns the data as series of lists'''
+    '''Reads in the energy file and returns the data as series of lists''' 
 
     with open(input_file, 'r') as f:
         print('Reading input from file:', input_file)
@@ -9,7 +38,6 @@ def read_energy(input_file):
             line_num += 1
             if line[0] != '#':
                 new_data = line.split()
-                print(new_data)
                 if len(new_data) == 4:
                     idx.append(new_data[0])
                     pos.append(new_data[1])
@@ -28,6 +56,13 @@ def write_output(output_file, index, time, position, velocity):
         for line in range(len(index)):
             f.write('{0} {1:0.06f} {2:0.06f} {3:0.06f}\n'.format(index[line], time[line], position[line], velocity[line])) #TODO fancier float format
 
+def main():
+    args = get_arguments()
+    idx, pos, pot, force = read_energy(args.energy)
+    print(args)
+    print(pos[5])
 
+if __name__ == '__main__':
+    main()
 
 
