85e0c291c90c9f1d2bdfcd75e21fa23a4440c1b3
Eric Holmgren <eholmgr2@u.rochester.edu>
2017-09-27 11:21:29 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from distutils.core import setup
 
 with open('README.md') as f:
-      long_description = ''.join(f.readlines())
+    long_description = ''.join(f.readlines())
 
 setup(name='LangevinSim',
-      version='0.01',
-      description='Langevin Simulator',
-      long_description=long_description,
-      author='Eric Holmgren',
-      author_email='eholmgr2@u.rochester.edu',
-      packages=['lans'],
-     )
+    version='0.01',
+    description='Langevin Simulator',
+    long_description=long_description,
+    author='Eric Holmgren',
+    author_email='eholmgr2@u.rochester.edu',
+    packages=['lans'],
+    entry_points=
+    {
+        'console_scripts': ['langevin=lans.lans:start']
+    }
+    )
